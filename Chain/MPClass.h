6471e4a6519048c8d36b6249028cf63767f9f0a1
maniac911 <465056269@qq.com>
2016-11-08 08:52:04 +0800

    second push

---
+++ Chain/MPClass.h
@@ -0,0 +1,13 @@
+//
+//  MPClass.h
+//  Chain
+//
+//  Created by DaVinci on 2016/11/8.
+//  Copyright © 2016年 DaVinci. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MPClass : NSObject
+
+@end
---
+++ Chain/MPClass.m
@@ -0,0 +1,13 @@
+//
+//  MPClass.m
+//  Chain
+//
+//  Created by DaVinci on 2016/11/8.
+//  Copyright © 2016年 DaVinci. All rights reserved.
+//
+
+#import "MPClass.h"
+
+@implementation MPClass
+
+@end
